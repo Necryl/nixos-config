5752d1cf99ba44be27b68abf6e1a4ac72709dc12
Necryl <74096664+Necryl@users.noreply.github.com>
2025-04-28 22:01:15 +0530

    update and ref hardware-configuration in etc/nixos

--- configuration.nix
+++ configuration.nix
@@ -21,7 +21,7 @@ in
 {
   imports = [
     # Include the results of the hardware scan.
-    ./hardware-configuration.nix
+    /etc/nixos/hardware-configuration.nix
   ];
 
   nix.settings.experimental-features = [
