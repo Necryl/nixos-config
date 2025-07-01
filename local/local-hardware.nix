5e736d050afcaf41726204ae7ac3dd4147a8532e
Necryl <74096664+Necryl@users.noreply.github.com>
2025-07-01 16:00:36 +0530

    add local hardware config template

---
+++ local/local-hardware.nix
@@ -0,0 +1,19 @@
+{
+  fileSystems."/" = {
+    device = "/dev/disk/by-label/nixos-root";
+    fsType = "ext4";
+  };
+
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-label/nixos-boot";
+    fsType = "vfat";
+    options = [
+      "fmask=0077"
+      "dmask=0077"
+    ];
+  };
+
+  swapDevices = [
+    { device = "/dev/disk/by-partlabel/nixos-swap"; }
+  ];
+}
