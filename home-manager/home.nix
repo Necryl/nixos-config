f1b9ff0748d1ea65e07207aa7caf43162954a3b1
Necryl <74096664+Necryl@users.noreply.github.com>
2025-07-04 10:33:41 +0530

    unhide local file

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -53,4 +53,15 @@
     # PATH = "$HOME/.local/bin:$PATH";
   };
 
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "application/vnd.oasis.opendocument.spreadsheet" = "org.gnumeric.gnumeric.desktop";
+      "application/vnd.ms-excel" = "org.gnumeric.gnumeric.desktop";
+      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" =
+        "org.gnumeric.gnumeric.desktop";
+      "text/csv" = "org.gnumeric.gnumeric.desktop";
+    };
+  };
+
 }
