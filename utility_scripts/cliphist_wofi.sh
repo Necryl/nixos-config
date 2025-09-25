b55499132594cc69d72626dcd33bae9f61e7d01d
Necryl <74096664+Necryl@users.noreply.github.com>
2025-09-25 23:31:37 +0530

    add quotes around cliphist script command

--- utility_scripts/cliphist_wofi.sh
+++ utility_scripts/cliphist_wofi.sh
@@ -1,2 +1,2 @@
-# bash /home/necryl/nixos-config/utility_scripts/cliphist_wofi.sh
+# bash "/home/necryl/nixos-config/utility_scripts/cliphist_wofi.sh"
 cliphist list | wofi --dmenu --prompt "Clipboard:" --insensitive | cliphist decode | wl-copy
