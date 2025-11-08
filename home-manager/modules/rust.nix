36640f75074fb2a2e6654763b0da0b077eedde2d
Necryl <74096664+Necryl@users.noreply.github.com>
2025-11-08 18:37:33 +0530

    remove redundant package to resolve conflicting paths issue

--- home-manager/modules/rust.nix
+++ home-manager/modules/rust.nix
@@ -5,9 +5,8 @@
 {
   home.packages = with pkgs; [
     rustup
-    rust-analyzer
+    # rust-analyzer rustup was already providing this, so commented it out to fix the conflicting paths issue
     wasm-pack
     wasm-bindgen-cli
-    # add other tools you want available globally
   ];
 }
